be940fd7f5a7ca634bb05f95bf19cde2fc92a629
Jorge Vergara <javebratt@gmail.com>
2016-10-27 09:26:17 -0500

    Adds share functionality

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { SocialSharing } from 'ionic-native';
 
 @Component({
   selector: 'page-home',
@@ -10,6 +11,38 @@ export class HomePage {
 
   constructor(public navCtrl: NavController) {
     
+  }
+
+  /**
+   * Opens up the share sheet so you can share using the app you like the most.
+   */
+  regularShare(){
+    // share(message, subject, file, url)
+    SocialSharing.share("Testing, sharing this from inside an app I'm building right now", null, "www/assets/img/hulk.jpg", null); 
+  }
+
+  /**
+   * This share's directly via twitter using the:
+   * shareViaTwitter(message, image, url)
+   */
+  twitterShare(){
+    SocialSharing.shareViaTwitter("Testing, sharing this from inside an app I'm building right now", "www/assets/img/hulk.jpg", null); 
+  }
+
+  /**
+   * This share's directly via whatsapp using the:
+   * shareViaWhatsapp(message, image, url)
+   */
+  whatsappShare(){
+    SocialSharing.shareViaWhatsApp("Testing, sharing this from inside an app I'm building right now", "www/assets/img/hulk.jpg", null); 
+  }
+
+  /**
+   * This share's directly via Instagram using:
+   * shareViaInstagram(message, image)
+   */
+  instagramShare(){
+    SocialSharing.shareViaInstagram("Testing, sharing this from inside an app I'm building right now", "www/assets/img/hulk.jpg"); 
   }
 
 }
